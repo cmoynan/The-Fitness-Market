3f89920554cd0688e6cb38d767fde061f1502bc9
Chris Moynan <Chrismoynan@outlook.com>
2025-01-17 21:00:01 +0000

    Added custom 404 html page

--- home/views.py
+++ home/views.py
@@ -8,3 +8,6 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def custom_404_view(request, exception):
+    return render(request, 'errors/404.html', status=404)
--- the_fitness_market/urls.py
+++ the_fitness_market/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404
+
+handler404 = 'home.views.custom_404_view'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
