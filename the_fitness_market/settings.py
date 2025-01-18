8435edb2dc5c62362e0916b31b094209b62f0d5b
Chris Moynan <Chrismoynan@outlook.com>
2025-01-18 00:47:44 +0000

    Configured static files for deployment with STATIC_ROOT and whitenoise

--- the_fitness_market/settings.py
+++ the_fitness_market/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'the_fitness_market.urls'
@@ -175,6 +176,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
